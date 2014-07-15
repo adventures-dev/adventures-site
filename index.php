405689fd78a307644a66e4c10aba10746f613e3d
Dave Oster <daveosterjr@gmail.com>
2014-07-15 15:26:46 -0500

    adding logos to site

--- index.php
+++ index.php
@@ -181,8 +181,24 @@
 					</div>
 					<div class="span4">
 						<div class="company_wrapper">
-							<div class="company" id="stockcastit">
-								<a href="http://www.stockcast.it" class="tool" data-toggle="tooltip" title="Uses machine learning and natural language processing technology to analyze Internet data, giving clear, concise, and actionable insights into what people feel about financial markets."><img src="img/stockcastit-white.png"></a>
+							<div class="company" id="copymint">
+								<a href="http://www.copymint.com" class="tool" data-toggle="tooltip" title="A (social) marketplace for content creators and publications."><img src="img/copymint.png"></a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row-fluid">
+					<div class="span6">
+						<div class="company_wrapper">
+							<div class="company" id="peoplekit">
+								<a href="http://www.peoplekit.co" class="tool" data-toggle="tooltip" title="PeopleKit delivers tools to support your team in working better together and increasing your bottom line."><img src="img/peoplekit.png"></a>
+							</div>
+						</div>
+					</div>
+					<div class="span6">
+						<div class="company_wrapper">
+							<div class="company" id="9thandelm">
+								<a href="http://www.9thandelm.com" class="tool" data-toggle="tooltip" title=""><img src="img/9thandelm.png"></a>
 							</div>
 						</div>
 					</div>
